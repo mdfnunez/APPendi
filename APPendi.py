ed89d873178f33dd385d7347b61337048036b4e6
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-02-24 14:54:07 -0600

    Update APPendi.py

--- APPendi.py
+++ APPendi.py
@@ -9,7 +9,7 @@ st.subheader("Protocolo de estudio de pacientes con apendicitis aguda, IMSS")
 #st.button("A")
 #Barra lateral, se puede colocar checkbox, texto, slider, etc.
 
-
+st.image(Especialidades.jpg)
 
 #BARRA LATERAL
 st.subheader("Panel de busqueda y registro")
