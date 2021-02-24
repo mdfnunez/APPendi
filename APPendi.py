3b33746fd03576c719062db40600611d0eca861f
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-02-24 17:18:50 -0600

    Update APPendi.py

--- APPendi.py
+++ APPendi.py
@@ -1,7 +1,13 @@
-#APPendi: para la comparacion del registro electronico vs juicio clinico en el diagnostico correcto de apendicitis aguda
+#excel
+import openpyxl
+from openpyxl import Workbook
+# #APPendi: para la comparacion del registro electronico vs juicio clinico en el diagnostico correcto de apendicitis aguda
 import streamlit as st
 from PIL import Image
-st.image("https://github.com/mdfnunez/APPendi/blob/main/APPendi.jpg?raw=true")
+#st.image("https://github.com/mdfnunez/APPendi/blob/main/APPendi.jpg?raw=true")
+#st.image("https://github.com/mdfnunez/APPendi/blob/main/Especialidades.jpg?raw=true"
+st.image("APPendi.jpg")
+st.title("Centro Médico Nacional Siglo XXI")
 st.subheader("Protocolo de estudio de pacientes con apendicitis aguda, IMSS")
 st.write("")
 st.subheader("Registro de nuevo paciente")
@@ -10,4 +16,19 @@ Edad=st.number_input("Edad")
 fechanac=st.date_input("Fecha de nacimiento")
 Nss=st.number_input("Numero de seguridad social")
 Genero=st.text_input("Genero")
-st.image("https://github.com/mdfnunez/APPendi/blob/main/Especialidades.jpg?raw=true")
+guardar=open
+
+
+#excel
+book=Workbook()
+sheet=book.active
+
+sheet["A1"]="Nombre"
+sheet["B1"]="Edad"
+sheet["C1"]="Fecha de nacimiento"
+sheet["D1"]="NSS"
+#cambiar color de leta
+#sheet["B1"].font=Font(color="FF0000",bold=True)  color rojo
+book.save("otra prueba.xlsx")
+#crear otra hoja
+#sheet2=book.create_sheet("hoja2")
