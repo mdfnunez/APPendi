508bf0994453cce986dc98631346fb5998080610
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-02-24 15:10:15 -0600

    Update APPendi.py

--- APPendi.py
+++ APPendi.py
@@ -3,7 +3,7 @@
 import streamlit as st
 from PIL import Image
 
-st.image("https://drive.google.com/file/d/1csunkvUKWZiIqSrDKch2hnsD9DVKtoRz/view?usp=sharing")
+st.image("https://github.com/mdfnunez/APPendi/blob/main/APPendi.jpg")
 st.title("Centro Medico Hola amor Siglo XXI")
 st.subheader("Protocolo de estudio de pacientes con apendicitis aguda, IMSS")
 #Escribir palabras simples sin formato
