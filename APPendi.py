b6cd5baa8c362897bf4f6d532e332f3a0492e0df
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-03-02 19:23:25 -0600

    Update APPendi.py

--- APPendi.py
+++ APPendi.py
@@ -1,46 +1,43 @@
-#excel
-import openpyxl
-import pandas as pd
-import numpy as np
+import sqlite3
+import pandas
+print("Bienvenido, iniciemos "
+      ""
+      ""
+      "")
+
+print("Registra tus datos")
+c=sqlite3.connect("Alpha.db")
+con=c.cursor()
 
 
-from openpyxl import Workbook
-# #APPendi: para la comparacion del registro electronico vs juicio clinico en el diagnostico correcto de apendicitis aguda
-import streamlit as st
-from PIL import Image
-#st.image("https://github.com/mdfnunez/APPendi/blob/main/APPendi.jpg?raw=true")
-#st.image("https://github.com/mdfnunez/APPendi/blob/main/Especialidades.jpg?raw=true"
-st.image("APPendi.jpg")
-st.title("Centro Médico Nacional Siglo XXI")
-st.subheader("Protocolo de estudio de pacientes con apendicitis aguda, IMSS")
-st.write("")
-st.subheader("Registro de nuevo paciente")
-Nombre=st.text_input("Nombre")
-Edad=st.number_input("Edad")
-fechanac=st.date_input("Fecha de nacimiento")
-Nss=st.number_input("Numero de seguridad social")
-Genero=st.text_input("Genero")
+def nuevatabla():
+      c
+      c.execute(""" CREATE TABLE IF NOT EXISTS pa (
+      NSS INTEGER PRIMARY KEY UNIQUE,
+      Nombre TEXT,
+      Edad INTEGER
+      )""")
+      c.commit()
+      c.close()
 
+#### agregar datos a la base de datos
+try:
+      NSS=int(input("Numero de seguridad social: "))
+      Nombre=input("Nombre del paciente: ")
+      Edad=int(input("Edad del paciente"))
 
-#excel
-book=Workbook()
-sheet=book.active
+      c.execute("INSERT INTO pa(NSS,Nombre,Edad)VALUES (?,?,?)""", (NSS,Nombre,Edad))
 
-sheet["A1"]="Nombre"
-sheet["B1"]="Edad"
-sheet["C1"]="Fecha de nacimiento"
-sheet["D1"]="NSS"
-#cambiar color de leta
-#sheet["B1"].font=Font(color="FF0000",bold=True)  color rojo
-book.save("https://github.com/mdfnunez/APPendi/blob/main/otra%20prueba.xlsx")
-#crear otra hoja
-#sheet2=book.create_sheet("hoja2")
+      c.commit()
+      print("Registro existoso")
+except :
+       print("Existe un registro previo con ese numero de seguridad social")
 
-#leer excel
-#book=openpyxl.load_workbook("Nombre de excel para abrir")
-#sheet=book.active
+def get_posts():
+    c.execute("SELECT * FROM pa")
+    print(con.fetchall())
 
-#salvar archivos subidos, incompleto
-# archivossubidos=st.file_uploader("Subir archivo")
-#with open(archivossubidos,"wb") as f:
-   # f.write(archivossubidos.getbuffer())
+get_posts()
+
+c.close()
+
