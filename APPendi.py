1e58578a69a49293e1a8d278e1a736169aeaa81b
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-03-02 19:58:37 -0600

    Update APPendi.py

--- APPendi.py
+++ APPendi.py
@@ -24,9 +24,9 @@ def nuevatabla():
 
 #### agregar datos a la base de datos
 try:
-      NSS=int(st.input_text("Numero de seguridad social: "))
-      Nombre=st.input_text("Nombre del paciente: ")
-      Edad=int(st.input_text("Edad del paciente"))
+      NSS=int(st.text_input("Numero de seguridad social: "))
+      Nombre=st.text_input("Nombre del paciente: ")
+      Edad=int(st.text_input("Edad del paciente"))
 
       c.execute("INSERT INTO pa(NSS,Nombre,Edad)VALUES (?,?,?)""", (NSS,Nombre,Edad))
 
