7921f665f523a89abe53380ee7a16f5b0014cb36
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2021-03-04 13:09:06 -0600

    Update APPendi.py

--- APPendi.py
+++ APPendi.py
@@ -1,45 +1,23 @@
 import sqlite3
-import pandas
 import streamlit as st
-st.write("Bienvenidos")
-print("Bienvenido, iniciemos "
-      ""
-      ""
-      "")
 
-print("Registra tus datos")
-c=sqlite3.connect("Alpha.db")
+st.title("Bienvenido")
+st.image("https://github.com/mdfnunez/APPendi/blob/main/APPendi.jpg")
+c=sqlite3.connect("Omega.db")
 con=c.cursor()
+creartabla=c.execute("""CREATE TABLE IF NOT EXISTS Theta(
+Nombre TEXT,
+Edad INTEGER,
+PeliculaFavorita TEXT)""")
+c.commit()
+st.cache()
+Nombre=st.text_input("Nombre: ")
+Edad=st.number_input("Edad: ")
+Pelifav=st.text_input("Pelicula favorita")
+Aplicar=st.button("Salvar cambios")
+if Aplicar==True:
+    c.execute("INSERT INTO Theta(Nombre,Edad,Peliculafavorita)VALUES (?,?,?)""", (Nombre, Edad, Pelifav))
+    c.commit()
+    c.close()
 
-
-def nuevatabla():
-      c
-      c.execute(""" CREATE TABLE IF NOT EXISTS pa (
-      NSS INTEGER PRIMARY KEY UNIQUE,
-      Nombre TEXT,
-      Edad INTEGER
-      )""")
-      c.commit()
-      c.close()
-
-#### agregar datos a la base de datos
-try:
-      NSS=st.text_input("Numero de seguridad social: ")
-      Nombre=st.text_input("Nombre del paciente: ")
-      Edad=st.text_input("Edad del paciente")
-
-      c.execute("INSERT INTO pa(NSS,Nombre,Edad)VALUES (?,?,?)""", (NSS,Nombre,Edad))
-
-      c.commit()
-      print("Registro existoso")
-except :
-       print("Existe un registro previo con ese numero de seguridad social")
-
-def get_posts():
-    c.execute("SELECT * FROM pa")
-    print(con.fetchall())
-
-get_posts()
-
-c.close()
 
